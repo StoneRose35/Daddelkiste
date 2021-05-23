c7be16c58e51174c3aef191bd213b9c11e9c97e8
Philipp Fuerholz <fuerholz@gmx.ch>
2021-05-23 11:41:20 +0200

    added battery voltage display

--- lcdInterface.py
+++ lcdInterface.py
@@ -10,9 +10,10 @@ import RPi.GPIO as GPIO
 
 SERIAL_DEVICE = "/dev/ttyACM0"
 GPIO_OFFSWITCH_PIN = 11
+BAT_CONVERSION = 0.011695888188
 
 arduino = serial.Serial(SERIAL_DEVICE)
-
+battery_voltage = 12.
 
 def convert_value(val):
     res = None
@@ -25,11 +26,12 @@ def convert_value(val):
 
 def read_serial_values():
     rawdata = arduino.readline()
+    # print(rawdata.decode("utf-8"))
     return rawdata.decode("utf-8")
 
 def set_volume(vol: int):
-    subprocess.call(["amixer","set", "Headphone", "{:.1f}%".format((vol*100)/1024)])
-
+    subprocess.call(["su","pi","-c", '"pactl set-sink-volume @DEFAULT_SINK@ {:0.0f}%"'.format((vol*100.)/1024.)])
+    #subprocess.call(["amixer","set","Master", "{:.1f}%".format((vol*100)/1024)])
 def get_cpu_temp():
     resp = subprocess.Popen(["vcgencmd", "measure_temp"], stdout=subprocess.PIPE)
     tempstring = str(resp.stdout.read().decode("utf-8"))
@@ -39,18 +41,29 @@ def get_cpu_temp():
     return None
 
 def serial_listener():
+    last_vol = 0
+    global battery_voltage
     while True:
         keyVal = convert_value(read_serial_values())
         if keyVal is not None:
             if "VOL" in keyVal:
                 set_volume(keyVal["VOL"])
+                arduino.write("D1V: {:.1f}%\n".format(float(keyVal["VOL"])/10.24).encode("utf-8"))
+                if int(keyVal["VOL"]) > 0 and last_vol == 0:
+                    arduino.write("A1".encode("utf-8"))
+                elif int(keyVal["VOL"]) == 0 and last_vol > 0:
+                    arduino.write("A0".encode("utf-8"))
+                last_vol = int(keyVal["VOL"])
+            if "BAT" in keyVal:
+                battery_voltage = float(keyVal["BAT"])*BAT_CONVERSION
 
 def serial_writer():
     cpu_temp_old = 0.0
+    global battery_voltage
     while True:
         cpu_temp = get_cpu_temp()
         if cpu_temp is not None and abs(cpu_temp_old-cpu_temp) > 0.1:
-            tempv_array = list("D2Temp {:.1f}'C\n".format(cpu_temp).encode("utf-8"))
+            tempv_array = list("D2T: {:.1f}'C B: {:.1f}V\n".format(cpu_temp,battery_voltage).encode("utf-8"))
             deg_idx = tempv_array.index(39)
             tempv_array[deg_idx]=223
             arduino.write(bytes(tempv_array))
@@ -59,14 +72,14 @@ def serial_writer():
             elif cpu_temp > 70.0:
                 arduino.write("F255\n".encode("utf-8"))
             else:
-                arduino.write("F0".encode("utf-8"))
+                arduino.write("F0\n".encode("utf-8"))
         cpu_temp_old = cpu_temp
         current_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-        arduino.write("D3{}".format(current_time).encode("utf-8")
+        arduino.write("D3{}\n".format(current_time).encode("utf-8"))
         time.sleep(0.5)
 
 def turn_off(channel):
-    print("switching off")    
+    arduino.write("D0Daddelkiste stopping\n".encode("utf-8"))
     subprocess.call(["shutdown", "-h", "now"], shell=False)
 
 def init_gpio():
@@ -77,6 +90,7 @@ def init_gpio():
 
 if __name__ == "__main__":
     init_gpio()
+    arduino.write("D0Daddelkiste running\n".encode("utf-8"))
     t1 = threading.Thread(target=serial_listener)
     t1.start()
     serial_writer()
