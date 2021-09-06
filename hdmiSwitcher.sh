b64df4f789732da77987186577f1189ce49d2af8
Philipp Fuerholz <fuerholz@gmx.ch>
2021-09-06 21:22:14 +0200

    bugfixed the hdmi-Switcher

--- hdmiSwitcher.sh
+++ hdmiSwitcher.sh
@@ -6,26 +6,36 @@
 # Required-Stop: $remote_fs $syslog
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6 
-# END INIT INFO
+### END INIT INFO
 
-HDMI_STATUS=`tvservice -l | grep -o HDMI`
-if [[ $HDMI_STATUS = "HDMI" ]]
-then
-    if [[ `ls -l /boot/ | grep -o hdmi_screen` != "hdmi_screen" ]]
-    then
-        cp -f /boot/config_hdmi.txt /boot/config.txt
-        touch /boot/hdmi_screen
-        rm -f /boot/lcd_screen
-        echo "HDMI Display available, restarting to have hdmi as primary display"
-        reboot
-    fi
-else
-    if [[ `ls -l /boot/ | grep -o ldc_screen` != "lcd_screen" ]]
-    then
-        cp -f /boot/config_lcd.txt /boot/config.txt
-        touch /boot/lcd_screen
-        rm -f /boot/hdmi_screen
-        echo "Internal Display only, restarting with the matching config"
-        reboot
-    fi
-fi
+
+case "$1" in 
+    start)
+	HDMI_STATUS=`tvservice -l | grep -o HDMI`
+	if [[ $HDMI_STATUS = "HDMI" ]]
+	then
+            FILESTATE=`ls -l /boot/ | grep -o hdmi_screen`
+	    if [[ $FILESTATE != "hdmi_screen" ]]
+	    then
+		cp -f /boot/config_hdmi.txt /boot/config.txt
+		touch /boot/hdmi_screen
+		rm -f /boot/lcd_screen
+		echo "HDMI Display available, restarting to have hdmi as primary display"
+		reboot
+	    fi
+	else
+            FILESTATE=`ls -l /boot/ | grep -o lcd_screen`
+	    if [[ $FILESTATE != "lcd_screen" ]]
+	    then
+		cp -f /boot/config_lcd.txt /boot/config.txt
+		touch /boot/lcd_screen
+		rm -f /boot/hdmi_screen
+		echo "Internal Display only, restarting with the matching config"
+		reboot
+	    fi
+	fi
+     ;;
+    *)
+    echo "script only active when called with 'start'"
+    ;;
+esac
